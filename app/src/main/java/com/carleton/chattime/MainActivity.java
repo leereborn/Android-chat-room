c73f1ad587838c95a1db5126a05bd14b955d9341
Rui Li <leereborn92929@gmail.com>
2017-11-27 11:53:16 -0500

    Add login functionality

--- app/src/main/java/com/carleton/chattime/MainActivity.java
+++ app/src/main/java/com/carleton/chattime/MainActivity.java
@@ -1,20 +1,54 @@
 package com.carleton.chattime;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int RC_SIGN_IN = 1;
 
     private Button viewButton;
     private Button createButton;
+    //firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    // User is signed in
+                    Toast.makeText(MainActivity.this, "Already signed in!", Toast.LENGTH_SHORT).show();
+                }else{
+                    // User is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                                         new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),RC_SIGN_IN);
+                }
+            }
+        };
 
         viewButton=(Button) findViewById(R.id.button_view_rooms);
         createButton=(Button) findViewById(R.id.button_create_room);
@@ -34,5 +68,17 @@ public class MainActivity extends AppCompatActivity {
                 //startActivity(intent);
             }
         });
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if(mAuthStateListener != null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
     }
 }
